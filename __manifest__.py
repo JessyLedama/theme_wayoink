dbed66f530fb71b23760f6a3d7068190f9ec1682
Jayliste <jaylisteledama94@gmail.com>
2023-02-15 14:03:40 +0300

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Wayoink Theme',
+    'description': 'An Odoo theme for Wayoink website. .',
+    'version': '1.0',
+    'author': 'SIMI Technologies',
+    'category': 'Theme/Creative',
+
+    'depends': ['website', 'website_theme_install'],
+    'data': [
+    ],
+}
